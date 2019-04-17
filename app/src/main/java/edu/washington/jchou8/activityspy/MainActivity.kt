aab73d84247dd296aa2944255decb25d827638f5
jchou8 <joseph98c@gmail.com>
2019-04-16 19:28:05 -0700

    add logging

--- app/src/main/java/edu/washington/jchou8/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/jchou8/activityspy/MainActivity.kt
@@ -2,11 +2,48 @@ package edu.washington.jchou8.activityspy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate event fired")
+        if (savedInstanceState != null) {
+            Log.i(TAG, savedInstanceState.toString())
+        }
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain!")
     }
 }
